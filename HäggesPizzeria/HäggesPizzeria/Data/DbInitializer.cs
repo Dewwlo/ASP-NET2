41691dbd7737867f1b3f9b7db15673aed1d19fc3
Dewwlo <a.bjrnlund@gmail.com>
2017-08-21 13:16:38 +0200

    Initial commit

---
+++ HäggesPizzeria/HäggesPizzeria/Data/DbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using HäggesPizzeria.Models;
+using HäggesPizzeria.Models.Dish;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HäggesPizzeria.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            var aUser = new ApplicationUser();
+            aUser.UserName = "kund@test.com";
+            aUser.Email = "kund@test.com";
+            var r = userManager.CreateAsync(aUser, "PassWord1$").Result;
+
+            var adminRole = new IdentityRole { Name = "Admin" };
+            var roleResult = roleManager.CreateAsync(adminRole).Result;
+
+            var adminUser = new ApplicationUser();
+            adminUser.UserName = "admin@test.com";
+            adminUser.Email = "admin@test.com";
+            var adminUserResult = userManager.CreateAsync(adminUser, "Password1$").Result;
+
+            userManager.AddToRoleAsync(adminUser, "Admin");
+
+            if (context.Dishes.ToList().Count == 0)
+            {
+                var capricciosa = new Dish { Name = "Capricciosa" };
+                var margaritha = new Dish { Name = "Margaritha" };
+                var hawaii = new Dish { Name = "Hawaii" };
+                context.AddRange(capricciosa, margaritha, hawaii);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ HäggesPizzeria/HäggesPizzeria/Models/Dish/Dish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HäggesPizzeria.Models.Dish
+{
+    public class Dish
+    {
+        public int DishId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
