3b1f0264539248579c13ccfa6d9f82578374ee80
Dewwlo <a.bjrnlund@gmail.com>
2017-08-30 11:11:11 +0200

    Filter dish by category now works.

---
+++ HäggesPizzeria/HäggesPizzeria/Services/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HäggesPizzeria.Data;
+using HäggesPizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HäggesPizzeria.Services
+{
+    public class CategoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
