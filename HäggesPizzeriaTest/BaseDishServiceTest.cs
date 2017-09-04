3471f8f368e8d69472de6b3d91e4cdb87449294d
Dewwlo <a.bjrnlund@gmail.com>
2017-09-04 17:43:10 +0200

    Reconstructed project to fix azure continues deployment

---
+++ HäggesPizzeriaTest/BaseDishServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HäggesPizzeriaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
